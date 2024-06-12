13ce5d7e4f42edb0cd84141fc2a759237dd4383f
Robert Frimpong <frimpongrobert322@gmail.com>
2024-06-12 16:29:33 +0200

    Removed old files

--- CleanProject.ps1
+++
@@ -1,3 +0,0 @@
-Set-Location -Path "C:\Users\Admin\Documents\Git\Azure_Resume\backend\api"
-Remove-Item -Path "C:\Users\Admin\Documents\Git\Azure_Resume\backend\api\obj\Debug\net8.0\WorkerExtensions\WorkerExtensions.csproj" -ErrorAction SilentlyContinue
-Func host start
