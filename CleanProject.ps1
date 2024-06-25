429336878763f13ceb860e7503363b49060b4349
Robert Frimpong <frimpongrobert322@gmail.com>
2024-06-25 22:54:26 +0200

    Updated Counter.cs, GetResumeCounter.cs, local.setting.json

---
+++ CleanProject.ps1
@@ -0,0 +1,3 @@
+Set-Location -Path "C:\Users\Admin\Documents\Git\Azure_Resume\backend\api"
+Remove-Item -Path "C:\Users\Admin\Documents\Git\Azure_Resume\backend\api\obj\Debug\net8.0\WorkerExtensions\WorkerExtensions.csproj" -ErrorAction SilentlyContinue
+Func host start
