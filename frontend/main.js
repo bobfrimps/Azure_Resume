d673670481038d4a894ba28c50909f498951a837
Robert Frimpong <frimpongrobert322@gmail.com>
2024-06-25 23:20:13 +0200

    Added main.js script to index.html

--- frontend/main.js
+++ frontend/main.js
@@ -5,10 +5,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const functionApi = 'http://localhost:7071/api/GetResumeCounter'; // Replace with actual API endpoint
 
 const getVisitCount = () => {
-    let count = 30;
-    fetch(functionApi)
-        .then(response => response.json())
-        .then(response => {
+    let count = 1;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response => {
             console.log("Website called function API.");
             count = response.count;
             document.getElementById("counter").innerText = count;
