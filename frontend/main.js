0fd2c35c0eeafb76e869876a4fb21255d81646dd
Robert Frimpong <frimpongrobert322@gmail.com>
2024-07-07 19:38:59 +0200

    Added fucntionURL

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter'; // Replace with actual API endpoint
+const functionApiUrl = 'https://bobcounter.azurewebsites.net/api/GetResumeCounter?code=Aa1eRJj1_IcPGfVZXEwkWgHmv73_h0KMnqvv1KB_OOZgAzFuVXODng%3D%3D'
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter'; // Replace with actual API endpoint
 
 const getVisitCount = () => {
     let count = 1;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
             console.log("Website called function API.");
